4dcce63a0b8f89fc76e4df468383899ec58b635a
lucky10618 <lucky10618@gmail.com>
2026-09-16 23:17:36 +0900

    refactor: customers/forms.pyをcustomersディレクトリへ移動
    - ディレクトリ構造の整理のため

--- customers/forms.py
+++ customers/forms.py
@@ -2,8 +2,9 @@ from django import forms
 
 from .models import Customer
 
+
 # customer_create_form
 class CustomerCreateForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'email', 'phone_number', 'address', 'free_text']
+        fields = ["name", "email", "phone_number", "address", "free_text"]
